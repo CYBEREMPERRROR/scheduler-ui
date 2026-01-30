52502126c9dd02bcdd4a192a724f21d71a095a90
Science Scheduler Team <scheduler@sciencefaculty.edu>
2026-01-31 00:56:59 +0100

    Update App.js for separate lecturer/student routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LecturerForm from "./components/LecturerForm";
 import StudentCalendar from "./components/StudentCalendar";
 
 function App() {
   return (
-    <div style={{padding:"20px"}}>
-      <LecturerForm />
-      <hr />
-      <StudentCalendar />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/lecturer">Lecturer</Link> | 
+        <Link to="/student">Student</Link>
+      </nav>
+      <Routes>
+        <Route path="/lecturer" element={<LecturerForm />} />
+        <Route path="/student" element={<StudentCalendar />} />
+        <Route path="*" element={<div>Welcome to Science Scheduler</div>} />
+      </Routes>
+    </Router>
   );
 }
 
